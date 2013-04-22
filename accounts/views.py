875d058b02c110583790a962665991d0bfbf48c5
Philip S Doctor <philip.s.doctor@gmail.com>
2013-04-21 19:41:26 -0500

    Minor cleanup on some imports and refactoring add_account template to use base.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404, render_to_response
 from accounts.models import Account
@@ -6,17 +6,17 @@ from django.template import RequestContext
 from django.db import transaction
 from accounts.forms import AccountForm, AccountType
 from fm.models import Tag
-from django.forms.models import model_to_dict
 
 
 def index(request):
+    """Returns list of accounts"""
     account_list = Account.objects.order_by('account_name')
     return render(request, 'accounts/account_list.html', {'account_list': account_list})
 
 
 def account_detail(request, account_id):
     """Returns account details"""
-    account = Account.objects.get(id=account_id)
+    account = get_object_or_404(Account, pk=account_id)
     form = AccountForm.get_AccountForm(account)
 
     return render_to_response("accounts/account_detail.html", {
